83ba86826afa28a3089ecc5aa191cbc2d3f5fe8c
Aaron <aaron.hoskins@mobileappscompany.com>
2019-01-08 12:03:36 -0500

    Initial commit

---
+++ app/src/main/java/examples/aaronhoskins/com/week1day2demo/SampleJavaClass.java
@@ -0,0 +1,8 @@
+package examples.aaronhoskins.com.week1day2demo;
+
+public class SampleJavaClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
